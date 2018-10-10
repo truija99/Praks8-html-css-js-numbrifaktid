6ccfe1db446d8299ad04077d4b0545650a48b077
Truija99 <truijam@gmail.com>
2018-10-10 16:26:29 +0300

    Lisatud inkrement.

--- main.js
+++ main.js
@@ -1,16 +1,38 @@
 // Listening to events
 let numInput = document.querySelector ('#numInput'); 
 let factOutput = document.querySelector ('#factOutput');
+let incInput = document.querySelector('#incInput');
 
 const getFact = () => {
+		factOutput.innerHTML = ''
+
+
 	let number = numInput.value;
-		if (number != '' )
-			axios.get('http://numbersapi.com/' + number)
+	let inc = incInput.value;
+	
+	let baseUrl = 'http://numbersapi.com/'
+	// Vaja saada sarnane URL
+	// http://numbersapi.com/1..3
+	let url = `${baseUrl}${number}..${parseInt(number) + parseInt (inc)}`;
+
+
+		if (number != '' && inc !='') {
+			axios.get(url)
 		.then(response => {
-			factOutput.innerText = response.data;
+			for (let item in response.data) {
+				let li = document.createElement('li');
+				li.innerText = response.data[item];
+
+				factOutput.appendChild(li);
+			}
 		})
 		.catch(error => {
 			console.log(error);
 		})
+	}
+	
+		factOutput.innerHTML = '';
 }
+
 numInput.addEventListener('input', getFact);
+incInput.addEventListener('input', getFact);
